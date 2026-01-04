fadd4462ec9e7402cc354b873ccfe49733fe9689
Simon <simsimland2004@gmail.com>
2026-01-04 15:22:59 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { AppRouter } from './routes/AppRouter';
+import { Sidebar } from './features/shared/components/Sidebar';
+import { TopBar } from './features/shared/components/TopBar';
+
+function App() {
+  return (
+    <div className="flex min-h-screen bg-slate-900 font-sans">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <TopBar />
+        <main className="flex-1 overflow-auto bg-slate-950">
+          <AppRouter />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
