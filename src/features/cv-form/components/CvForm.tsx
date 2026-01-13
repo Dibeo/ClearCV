e92a803e87d66d93468c3ed512ef0727c613ac37
Simon <simsimland2004@gmail.com>
2026-01-13 21:08:35 +0100

    fix: forgot form eductaion field

--- src/features/cv-form/components/CvForm.tsx
+++ src/features/cv-form/components/CvForm.tsx
@@ -5,17 +5,19 @@ import { FormExperiences } from "./FormExperiences";
 import { FormSkills } from "./FormSkills";
 import { FormLanguages } from "./FormLanguages";
 import { FormCertifications } from "./FormsCertifications";
+import { FormEducations } from "./FormEducation";
 
 export const CvForm = () => {
   return (
     <div className="max-w-xl mx-auto space-y-12 pb-20">
       <FormIdentity />
       <FormContacts />
-      <FormSocials />
-      <FormSkills />
+      <FormEducations />
       <FormExperiences />
       <FormLanguages />
       <FormCertifications />
+      <FormSocials />
+      <FormSkills />
     </div>
   );
 };
