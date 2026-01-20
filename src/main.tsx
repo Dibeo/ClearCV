d2fe985d2aafd2654e4e86db53590b365159dc08
Simon <simsimland2004@gmail.com>
2026-01-20 18:42:16 +0100

    feat: add loading screen while charging language

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' 
-import { HashRouter } from 'react-router-dom'
-import './i18n.ts'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import "./i18n.ts";
+import { LoadingScreen } from "./features/shared/components/Loading.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <Suspense fallback="Initialisation...">
+      <Suspense fallback={<LoadingScreen />}>
         <App />
       </Suspense>
     </HashRouter>
   </React.StrictMode>,
-)
+);
